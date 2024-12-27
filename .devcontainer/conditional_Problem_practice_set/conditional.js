5847c15cc7557cb2ee0ad5391107720c3a12d84d
jyotirmay mishra <jyotirmay.cot@gmail.com>
2024-12-27 06:12:53 +0000

    Container Config and switch-case-example in conditional.js

---
+++ .devcontainer/conditional_Problem_practice_set/conditional.js
@@ -0,0 +1,15 @@
+let age = 14;
+switch(age) {
+    case 11:
+        console.log('your age is 11')
+    case 12:
+        console.log('your age is 12')
+    case 13:
+        console.log('your age is 13')
+    case 14:
+        console.log('your age is 14')
+    case 15:
+        console.log('your age is 15')
+    default:
+        console.log('age is :'+age)
+}
