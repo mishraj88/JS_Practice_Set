7b3dd14a3350818cf5e01d1dfaf6bc8c86ec2f2b
jyotirmay mishra <jyotirmay.cot@gmail.com>
2024-12-27 06:22:37 +0000

    if-elese-if example

---
+++ .devcontainer/conditional_Problem_practice_set/if-else-if-else-example.js
@@ -0,0 +1,8 @@
+let age = 10;
+console.log("Enterred age: "+ age)
+if(age < 9)
+    console.log('You are too small in age to think about driving car, it is dangerous! Go and play some games')
+else if(age >=10 && age < 18)
+    console.log('You are still young to be driving cars. You won\'t be given DL')
+else
+    console.log('If you have a valid DL, you can drive! Be save and enjoy your drive!')
--- .devcontainer/conditional_Problem_practice_set/switch-case-example.js
+++ .devcontainer/conditional_Problem_practice_set/switch-case-example.js
@@ -2,14 +2,19 @@ let age = 14;
 switch(age) {
     case 11:
         console.log('your age is 11')
+        break
     case 12:
         console.log('your age is 12')
+        break
     case 13:
         console.log('your age is 13')
+        break
     case 14:
         console.log('your age is 14')
+        break
     case 15:
         console.log('your age is 15')
+        break
     default:
         console.log('age is :'+age)
 }
